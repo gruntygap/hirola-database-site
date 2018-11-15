6f1f743cf28f41c65a4b1cf96a6659fef2f1bda3
Grant Gapinski <gruntygap@gmail.com>
2018-11-14 22:35:12 -0600

    Added Mascot

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def landing_page():
 	# pass in the classes within database for add-section-course
 	# place that where steve is
     return render_template('index.html', steve="steve is a good boi")
+
 
 @app.route("/somewhere_else", methods=['POST'])
 def receive_post():
@@ -15,3 +17,12 @@ def receive_post():
 		print(key)
 		print(value)
 	return render_template('show_data_sent.html', recieve=recieve)
+
+
+def connect_to_db():
+	pass
+
+
+def make_query(query):
+	pass
+
