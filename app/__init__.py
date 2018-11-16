221482ed4c19f982e7b62c21f7815aa03cd487d2
Grant Gapinski <gruntygap@gmail.com>
2018-11-16 12:19:44 -0600

    Changing up the front end ;) doing some db-connector stuff...

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import config
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -7,39 +9,44 @@ app = Flask(__name__)
 def landing_page():
 	# pass in the classes within database for add-section-course
 	# place that where steve is
-    return render_template('dashboard.html', steve="steve is a good boi")
-
-
-@app.route('/dashboard')
-def dash():
-	# pass in the classes within database for add-section-course
-	# place that where steve is
-    return landing_page()
+	return render_template('dashboard.html', steve="steve is a good boi")
 
 
 @app.route('/functions')
 def function_page():
-    return render_template('input_functions.html')
+	return render_template('input_functions.html')
 
 
 @app.route('/time-warps')
 def time_warps():
-    return render_template('time_warps.html')
+	return render_template('time_warps.html')
 
 
 @app.route("/somewhere_else", methods=['POST'])
 def receive_post():
-	recieve = request.form
-	for key, value in recieve.items():
+	connect_to_db()
+	receive = request.form
+	for key, value in receive.items():
 		print(key)
 		print(value)
-	return render_template('show_data_sent.html', recieve=recieve)
+	return render_template('show_data_sent.html', recieve=receive)
 
 
 def connect_to_db():
-	pass
+	cnx = mysql.connector.connect(user=config.DATABASE_USER,
+								  password=config.DATABASE_PASSWORD,
+								  host=config.DATABASE_ADDRESS,
+								  database=config.DATABASE_NAME)
+	cursor = cnx.cursor()
+
+	cursor.execute("select * from student;")
+
+	for (ID, name, dept_name, tot_cred) in cursor:
+		print("{}, {}, {}, {}".format(
+			ID, name, dept_name, tot_cred))
+	cursor.close()
+	cnx.close()
 
 
 def make_query(query):
 	pass
-
